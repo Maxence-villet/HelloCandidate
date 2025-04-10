8983d563337ef1e6229210e08adc36d1dd360d3f
Maxence.villet <maxence.villetschoumaker@gmail.com>
2025-04-10 11:56:09 +0200

    change icone of student dashboard

--- utils/header/header_student.php
+++ utils/header/header_student.php
@@ -15,6 +15,7 @@ $username = htmlspecialchars($_SESSION['username']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HelloCandidate - <?php echo htmlspecialchars($page_title ?? 'Dashboard'); ?></title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
 </head>
 <body class="bg-gray-50">
     <!-- Top Bar -->
--- utils/sidebar/sidebar_student.php
+++ utils/sidebar/sidebar_student.php
@@ -18,6 +18,12 @@ $username = isset($_SESSION['username']) ? htmlspecialchars($_SESSION['username'
     <nav>
         <ul class="space-y-2">
             <li>
+                <a href="/student/dashboard" class="flex items-center space-x-2 text-blue-600 hover:bg-blue-50 p-2 rounded">
+                    <i class="fas fa-tachometer-alt w-5 h-5"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li>
                 <a href="/profile" class="flex items-center space-x-2 text-blue-600 hover:bg-blue-50 p-2 rounded">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
